720f91aefd242bd8204f8bdb0ac8ba2a13dc8736
Alfonso Fernandez <le_debutant@yahoo.fr>
2017-07-11 02:38:42 +0200

    commentaires dans delphine.txt et services/signupServices.php

--- services/signupServices.php
+++ services/signupServices.php
@@ -5,6 +5,16 @@ $pseudo = $_POST['pseudo'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+
+/*
+ * Alfonso: Ceci devrait marcher mais il te reste des controles
+ * à faire c'est à dire sur le pseudo. Sur l'email et le password
+ * tu peut utiliser strlegnth strlen() pour la longueur d'un pseudo
+ * et mot de passe et preg_match() pour vérifier le format d'un email
+ * */
+
+
+
 $result = inscription($pseudo, $email, $password);
 
 if($result == 1){
